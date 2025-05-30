5a4d47f582611668776b28abda06f862a3d7ac56
unknown <akhmadjon.tursunov1@gmail.com>
2025-05-30 19:07:16 +0900

    'update'

--- src/logging.py
+++ src/logging.py
@@ -2,14 +2,20 @@ import logging
 import os
 from datetime import datetime
 
-LOG_LIFE=f"{datetime.now().strftime('%m_%d,%_Y,%_H,%_M,_%_S')}.log"
-log_path=os.path.join(os.getcwd(),"log",LOG_LIFE)
-os.makedirs(log_path, exist_ok=True)
+# Generate a timestamped log file name
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-LOG_LIFE_PATH=os.path.join(log_path, LOG_LIFE)
+# Create logs directory
+logs_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(logs_dir, exist_ok=True)
 
+# Full path to the log file
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+# Configure logging
 logging.basicConfig(
-    filename=LOG_LIFE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    filename=LOG_FILE_PATH,
+    filemode='a',  # 'a' means append mode
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )

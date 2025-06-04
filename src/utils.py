2dec41829b1603e3ad601777648945dd8ba81ff7
unknown <akhmadjon.tursunov1@gmail.com>
2025-06-04 14:54:14 +0900

    update

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,8 @@
 import os
 import sys
-import numpy as np
-import pandas as pd
 import dill
 import pickle
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 from src.logging import logging
@@ -15,5 +14,34 @@ def save_object(file_path, obj):
         with open(file_path, 'wb') as file_obj:
             dill.dump(obj, file_obj)
             
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try: 
+        with open(file_path, 'rb') as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def evaluate_model(X_train, y_train, X_test, y_test, model):
+    try:
+        report = {}
+
+        for name, model in model.items():
+            model.fit(X_train, y_train)
+            y_train_pred=model.predict(X_test)
+            y_test_pred=model.predict(X_test)
+
+            train_model_score=r2_score(y_train, y_train_pred)
+            test_model_score=r2_score(y_test, y_test_pred)
+
+            report[name] = test_model_score
+
+            return report
+        
     except Exception as e:
         raise CustomException(e, sys)

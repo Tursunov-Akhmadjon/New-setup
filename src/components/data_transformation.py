15d881fe226d403d5c9b9df5df0bc6a2eda6982c
unknown <akhmadjon.tursunov1@gmail.com>
2025-06-11 15:42:35 +0900

    'updates'

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -22,8 +22,8 @@ class DataTransformation:
     
     def get_data_transformation_obj(self):
         try:
-            numerical_columns=['reading score', 'writing score']
-            categorical_columns=['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course']
+            numerical_columns=['reading_score', 'writing_score']
+            categorical_columns=['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
 
             num_pipeline=Pipeline(
                 steps=[
@@ -66,8 +66,8 @@ class DataTransformation:
             logging.info('Obtaining Data Preprocessing objects')
 
             preprocessing_obj = self.get_data_transformation_obj()
-            target_column_name = 'math score'
-            numerical_columns = ['reading score', 'writing score']
+            target_column_name = 'math_score'
+            numerical_columns = ['reading_score', 'writing_score']
 
             input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
             target_feature_train_df = train_df[target_column_name]
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import pandas as pd
-import numpy
 from src.exception import CustomException
 from src.logging import logging
 from src.utils import load_object
@@ -10,15 +9,50 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self, features):
-        model_path = 'artifacts\model.pkl'
-        preprocessor_path = 'artifacts\preprocessing.pkl'
-        model=load_object(file_path=model_path)
+    def predict(self,features):
+        try:
+            model_path=os.path.join("artifacts","model.pkl")
+            preprocessor_path=os.path.join('artifacts','preprocessing.pkl')
+
+            logging.info("Model loaded for prediction")
+            model=load_object(file_path=model_path)
+            preprocessor=load_object(file_path=preprocessor_path)
+            
+            logging.info("Model predicts")
+            data_scaled=preprocessor.transform(features)
+            preds=model.predict(data_scaled)
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+
+
 
 class CustomData:
-    def __init__(self, 
-                 gender:str, 
-                 ):
-          self.gender = gender,
-    
-    def get_data_as_data_frame
+    def __init__(self, gender: str, race_ethnicity: str, parental_level_of_education: str,
+        lunch: str, test_preparation_course: str, reading_score: int, writing_score: int):
+
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score],
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+
+        except Exception as e:
+            raise CustomException(e, sys)
